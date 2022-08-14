80571d054ad733899ec64c36eccf10d38f7d60ca
Hwang <rsoft.hwang99@gmail.com>
2022-08-15 05:17:38 +0700

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import database from '@react-native-firebase/database';
+import firestore from '@react-native-firebase/firestore';
 
 export default function App() {
+
+  const writeDataBase = async () => {
+    try {
+      await firestore().collection('users').add({
+        name: "user name",
+        age: 23
+      })
+      console.log(`Success`)
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="Write to database" onPress={writeDataBase} />
     </View>
   );
 }
